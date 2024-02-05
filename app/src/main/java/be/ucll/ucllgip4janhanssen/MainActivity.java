d0ffa44a000bf28a29078ab56a22b696599411f1
Jan Hanssen <jan.hanssen@student.ucll.be>
2024-02-05 13:16:52 +0100

    Contacts lijst

--- app/src/main/java/be/ucll/ucllgip4janhanssen/MainActivity.java
+++ app/src/main/java/be/ucll/ucllgip4janhanssen/MainActivity.java
@@ -1,14 +1,21 @@
 package be.ucll.ucllgip4janhanssen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container_view);
+        navController = navHostFragment.getNavController();
     }
 }
